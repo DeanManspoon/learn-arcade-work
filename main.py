3f51814c10bff453eee6deab4f26f2957ab83178
Dean Mansell <DMansell@stu.kerikerihigh.ac.nz>
2021-08-13 09:33:19 +1200

    sir said

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 
 SPRITE_SCALING_BOX = 0.5
 SPRITE_SCALING_PLAYER = 0.5
+TILE_SCALING = 1
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -27,7 +28,6 @@ class MyGame(arcade.Window):
 
         self.player_list = None
         self.wall_list = None
-
         self.shrooms_list = None
         self.player_sprite = None
 
@@ -40,6 +40,47 @@ class MyGame(arcade.Window):
 
     def setup(self):
 
+        platforms_layer_name = 'Platforms'
+
+        coins_layer_name = 'Coins'
+
+        foreground_layer_name = 'Foreground'
+
+        background_layer_name = 'Background'
+
+        dont_touch_layer_name = "Don't Touch"
+
+
+        map_name = f"maps\level1.tmx"
+
+        my_map = arcade.tilemap.read_tmx(map_name)
+
+        self.end_of_map = my_map.map_size.width * SPRITE_SCALING_BOX
+
+        self.background_list = arcade.tilemap.process_layer(my_map,
+                                                           background_layer_name,
+                                                           TILE_SCALING)
+
+        self.foreground_list = arcade.tilemap.process_layer(my_map,
+                                                            foreground_layer_name,
+                                                            TILE_SCALING)
+
+
+        self.wall_list = arcade.tilemap.process_layer(map_object=my_map,
+                                                      layer_name=platforms_layer_name, 
+                                                     scaling=TILE_SCALING,
+                                                      use_spatial_hash=True)
+
+
+        self.tinyshroom_list = arcade.tilemap.process_layer(my_map,                               
+                                                      coins_layer_name,
+                                                     TILE_SCALING,
+                                                     use_spatial_hash=True)
+
+        self.dont_touch_list = arcade.tilemap.process_layer(my_map,
+                                                           dont_touch_layer_name,
+                                                           TILE_SCALING,
+                                                           use_spatial_hash=True)
         arcade.set_background_color(arcade.color.BANANA_YELLOW)
 
         self.view_left = 0
@@ -51,7 +92,7 @@ class MyGame(arcade.Window):
         self.score = 0
         self.shrooms_list = arcade.SpriteList()
         for x in range(20):
-            shrooms = arcade.Sprite("tinyshroom_red.png", SPRITE_SCALING_BOX)
+            shrooms = arcade.Sprite("images/tinyShroom_red.png", SPRITE_SCALING_BOX)
             shrooms.center_x = random.randint(1, SCREEN_WIDTH)
             shrooms.center_y = random.randint(1, SCREEN_HEIGHT)
             self.shrooms_list.append(shrooms)
@@ -64,14 +105,12 @@ class MyGame(arcade.Window):
 
     
         for coordinate in coordinate_list:
-            shrooms = arcade.Sprite("tinyshroom_red.png", SPRITE_SCALING_BOX)
+            shrooms = arcade.Sprite("images/tinyShroom_red.png", SPRITE_SCALING_BOX)
             shrooms.center_x = coordinate[0]
             shrooms.center_y = coordinate[1]
             self.shrooms_list.append(shrooms)
 
-
-# TODO - replace zombie with own sprite using code up top line 39
-        self.player_sprite = arcade.Sprite("zombie_cheer2.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite = arcade.Sprite("images\zombie_cheer2.png", SPRITE_SCALING_PLAYER)
         self.player_sprite.center_x = 300
         self.player_sprite.center_y = 300
         self.player_list.append(self.player_sprite)
